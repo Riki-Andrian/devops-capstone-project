eef4747bcb760dbd5be3629036cc853295365eb9
rikin <rikinugraha@student.telkomuniversity.ac.id>
2024-02-27 07:53:43 +0700

    Added CORS headers

--- service/__init__.py
+++ service/__init__.py
@@ -9,6 +9,7 @@ from flask import Flask
 from service import config
 from service.common import log_handlers
 from flask_talisman import Talisman
+from flask_cors import CORS
 
 # Create Flask application
 app = Flask(__name__)
@@ -38,3 +39,4 @@ except Exception as error:  # pylint: disable=broad-except
 app.logger.info("Service initialized!")
 
 talisman = Talisman(app)
+CORS(app)
